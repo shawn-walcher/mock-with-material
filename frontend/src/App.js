18be7355368344e9d172420cf06b214da18bcb21
Shawn Walcher <shawn.walcher@gmail.com>
2021-08-31 20:15:11 -0500

    basic page with material-ui core and icons added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Menu as MenuIcon } from '@material-ui/icons';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
-  return <div className='App'>Placeholder text for now</div>;
+  const classes = useStyles();
+
+  return (
+    <div className='App'>
+      <CssBaseline />
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu'>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            Placeholder site name for now
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+      Placeholder text for now
+    </div>
+  );
 }
 
 export default App;
