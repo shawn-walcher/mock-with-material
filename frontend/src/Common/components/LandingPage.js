949841016b6c52b3b213cb71e23b1fa3192f80e0
Shawn Walcher <shawn.walcher@gmail.com>
2021-09-09 17:19:23 -0500

    Updated landing page with a couple of navigation buttons

--- frontend/src/Common/components/LandingPage.js
+++ frontend/src/Common/components/LandingPage.js
@@ -1,6 +1,32 @@
+import { Button, Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { useHistory } from 'react-router';
+
 const LandingPage = () => {
-  // TODO: Create landing page
-  return <div>Landing Page</div>;
+  const history = useHistory();
+
+  return (
+    <div>
+      <Typography variant='h3' gutterBottom>Landing Page</Typography>
+      {/* TODO: Provide links for content and additional helpful landing page features */}
+      <Card variant='outlined'>
+        <CardContent>
+          Information for different subroutes (subapps)
+          <Grid container spacing={2}>
+            <Grid item>
+              <Button variant='contained' onClick={() => history.push('/tfe')}>
+                TFE Management
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant='contained' onClick={() => history.push('/health')}>
+                API Health
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default LandingPage;
