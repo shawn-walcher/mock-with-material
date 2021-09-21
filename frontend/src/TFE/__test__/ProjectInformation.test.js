f09f121cf1cde192e59cb5219b56aece8ce89c3e
Shawn Walcher <shawn.walcher@gmail.com>
2021-09-20 22:19:56 -0500

    Added additional tests

--- frontend/src/TFE/__test__/ProjectInformation.test.js
+++ frontend/src/TFE/__test__/ProjectInformation.test.js
@@ -1,27 +1,44 @@
 import { screen } from '@testing-library/react';
-import  ProjectInformation  from '../components/project/ProjectInformation';
+import ProjectInformation from '../components/project/ProjectInformation';
 import { renderWithContext } from '../../Context/__mocks__/renderWithContext';
 
 describe('Project Information card', () => {
-  
   test('contains Project Name', () => {
     renderWithContext(<ProjectInformation />);
     expect(screen.getByText('Project Name')).toBeInTheDocument();
-    expect(screen.getByText('mock-project-id-99')).toBeInTheDocument();
   });
   test('contains Ancestry', () => {
     renderWithContext(<ProjectInformation />);
     expect(screen.getByText('Ancestry')).toBeInTheDocument();
-    expect(screen.getByText('fake.net > tfe > project-id-99 > non-production')).toBeInTheDocument();
   });
   test('contains Owner Name', () => {
     renderWithContext(<ProjectInformation />);
     expect(screen.getByText('Owner Name')).toBeInTheDocument();
-    expect(screen.getByText('Jane Smith')).toBeInTheDocument();
   });
   test('contains Owner ID', () => {
     renderWithContext(<ProjectInformation />);
     expect(screen.getByText('Owner ID')).toBeInTheDocument();
-    expect(screen.getByText('js1234')).toBeInTheDocument();
+  });
+
+  describe('Given projectInformation', () => {
+    test('it displays the projectInformation.name', () => {
+      renderWithContext(<ProjectInformation />);
+      expect(screen.getByText('mock-project-id-99')).toBeInTheDocument();
+    });
+    test('it displays the projectInfo.ancestry', () => {
+      renderWithContext(<ProjectInformation />);
+      expect(screen.getByText('fake.net > tfe > project-id-99 > non-production')).toBeInTheDocument();
+    });
+  });
+
+  describe('Given projectOwner', () => {
+    test('it displays the projectOwner.name', () => {
+      renderWithContext(<ProjectInformation />);
+      expect(screen.getByText('js1234')).toBeInTheDocument();
+    });
+    test('it displays the projectOwner.id', () => {
+      renderWithContext(<ProjectInformation />);
+      expect(screen.getByText('Jane Smith')).toBeInTheDocument();
+    });
   });
 });
